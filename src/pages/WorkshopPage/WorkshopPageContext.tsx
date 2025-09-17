ea2c01289309688c7937b7f5223ffe56d1d33abc
Yue Peng <yp2841@nyu.edu>
2025-09-17 22:32:49 +0800

    chore: re-export WorkshopPageContext for compatibility

---
+++ src/pages/WorkshopPage/WorkshopPageContext.tsx
@@ -0,0 +1,5 @@
+export type { DirectoryContextType as WorkshopPageContextType } from "../../context/DirectoryContext";
+export {
+  DirectoryContext as WorkshopPageContext,
+  DirectoryContextProvider as WorkshopPageContextProvider,
+} from "../../context/DirectoryContext";
